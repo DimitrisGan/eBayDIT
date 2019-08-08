bb5c7520b2ec4cc9ef4045e8582cd2c1f966acd4
dimitris <jimgangas@gmail.com>
2019-08-08 20:20:09 +0300

    update main

--- src/main/java/com/ted/eBayDIT/EBayDitApplication.java
+++ src/main/java/com/ted/eBayDIT/EBayDitApplication.java
@@ -8,22 +8,49 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
 @SpringBootApplication
 public class EBayDitApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EBayDitApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EBayDitApplication.class, args);
+    }
 
 
-	@Bean
-	public BCryptPasswordEncoder encoder() {
-		return new BCryptPasswordEncoder();
-	}
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
-	@Bean
-	public SpringApplicationContext SpringApplicationContext()  {
-		return new SpringApplicationContext();
-	}
+    @Bean
+    public SpringApplicationContext SpringApplicationContext()  {
+        return new SpringApplicationContext();
+    }
+
+
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+
+        config.setExposedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Authorization, UserID, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"));
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
