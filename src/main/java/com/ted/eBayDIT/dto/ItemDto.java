7978f0e155040924b9cf301eeb9a3e02b7f79bac
dimitris <jimgangas@gmail.com>
2019-08-17 21:12:55 +0300

    itemDto class/file added

---
+++ src/main/java/com/ted/eBayDIT/dto/ItemDto.java
@@ -0,0 +1,10 @@
+package com.ted.eBayDIT.dto;
+
+import java.io.Serializable;
+
+public class ItemDto implements Serializable {
+    private static final long serialVersionUID = -6327622395341969676L;
+
+
+
+}
