f400d9ef867ac8ce6cd502c6c85160dc55c38bd9
dimitris <jimgangas@gmail.com>
2019-09-12 23:07:31 +0300

    added Visit Service file

---
+++ src/main/java/com/ted/eBayDIT/service/VisitService.java
@@ -0,0 +1,7 @@
+package com.ted.eBayDIT.service;
+
+
+public interface VisitService {
+
+
+}
--- src/main/java/com/ted/eBayDIT/service/impl/VisitServiceImpl.java
+++ src/main/java/com/ted/eBayDIT/service/impl/VisitServiceImpl.java
@@ -1,4 +1,11 @@
 package com.ted.eBayDIT.service.impl;
 
-public class VisitServiceImpl {
+import com.ted.eBayDIT.service.UserService;
+import com.ted.eBayDIT.service.VisitService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VisitServiceImpl implements VisitService {
+    
+
 }
